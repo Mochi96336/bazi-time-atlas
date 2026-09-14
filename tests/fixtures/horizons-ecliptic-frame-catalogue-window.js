faf0f380d8881cc6e8e8c3a84a28e9ea704d22f9
Mochi96336 <144137376+Mochi96336@users.noreply.github.com>
2026-09-15 02:55:19 +0800

    feat(astronomy): cover full year-4006 ecliptic frame

---
+++ tests/fixtures/horizons-ecliptic-frame-catalogue-window.js
@@ -0,0 +1,114 @@
+export const HORIZONS_4006_CATALOGUE_FRAME_SELECTED_CASES = Object.freeze([
+  Object.freeze({
+    label:"4005-12-01T12:00", jdTt:3184191, startJdTt:3184190.5, endJdTt:3184191.5,
+    researchResidualArcsec:0.0024696440536055922,
+    start:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([249.0035633,-0.0002017]), icrf:Object.freeze([218.49716513199999,-14.892919124000001]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([90.354556299999999,-4.4459013000000001]), icrf:Object.freeze([61.083214005000002,16.000910176000001]) }),
+    }),
+    truth:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([249.50217610000001,-0.00021019999999999999]), icrf:Object.freeze([218.988240558,-15.046271559999999]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([96.807498600000002,-4.7133399000000002]), icrf:Object.freeze([67.738053394000005,16.838866701000001]) }),
+    }),
+    end:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([250.0009234,-0.00021680000000000001]), icrf:Object.freeze([219.480155373,-15.198606841]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([103.18707620000001,-4.9185087999999997]), icrf:Object.freeze([74.363233758999996,17.45544761]) }),
+    }),
+  }),
+  Object.freeze({
+    label:"4005-12-21T12:00", jdTt:3184211, startJdTt:3184210.5, endJdTt:3184211.5,
+    researchResidualArcsec:0.00031759476973564136,
+    start:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([269.0510271,7.6000000000000004e-05]), icrf:Object.freeze([238.813345416,-20.100736888]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([350.93532240000002,3.1991491999999999]), icrf:Object.freeze([324.04507458699999,-10.777811818]) }),
+    }),
+    truth:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([269.55447179999999,6.19e-05]), icrf:Object.freeze([239.33843945300001,-20.202944128999999]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([357.9923326,2.6868283000000002]), icrf:Object.freeze([330.965695622,-8.9218460470000007]) }),
+    }),
+    end:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([270.05799730000001,4.6499999999999999e-05]), icrf:Object.freeze([239.864302122,-20.303606872]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([5.0081835999999997,2.136701]), icrf:Object.freeze([337.780196859,-6.9494155759999998]) }),
+    }),
+  }),
+  Object.freeze({
+    label:"4005-12-22T12:00", jdTt:3184212, startJdTt:3184211.5, endJdTt:3184212.5,
+    researchResidualArcsec:0.0029211212718926105,
+    start:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([270.05799730000001,4.6499999999999999e-05]), icrf:Object.freeze([239.864302122,-20.303606872]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([5.0081835999999997,2.136701]), icrf:Object.freeze([337.780196859,-6.9494155759999998]) }),
+    }),
+    truth:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([270.56160399999999,3.01e-05]), icrf:Object.freeze([240.39092714099999,-20.402713489]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([11.9819464,1.5578536000000001]), icrf:Object.freeze([344.50096439499998,-4.8915985170000003]) }),
+    }),
+    end:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([271.06529210000002,1.31e-05]), icrf:Object.freeze([240.91830825,-20.500252511999999]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([18.913437800000001,0.95954379999999995]), icrf:Object.freeze([351.14317819199999,-2.7789338020000001]) }),
+    }),
+  }),
+  Object.freeze({
+    label:"4006-02-11T12:00", jdTt:3184263, startJdTt:3184262.5, endJdTt:3184263.5,
+    researchResidualArcsec:0.0073005230874369611,
+    start:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([321.78031549999997,0.0002253]), icrf:Object.freeze([295.45566827800002,-21.160237034000001]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([310.67396930000001,4.8619830999999998]), icrf:Object.freeze([283.11820173699999,-17.770151335000001]) }),
+    }),
+    truth:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([322.28919089999999,0.0002185]), icrf:Object.freeze([295.99320147399999,-21.073643704999999]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([318.22327790000003,4.6341245999999998]), icrf:Object.freeze([290.98023901699997,-17.131085104]) }),
+    }),
+    end:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([322.79805260000001,0.00020939999999999999]), icrf:Object.freeze([296.53009307399998,-20.985364236999999]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([325.81250390000002,4.3232013]), icrf:Object.freeze([298.82310708300002,-16.185585140000001]) }),
+    }),
+  }),
+  Object.freeze({
+    label:"4006-10-20T12:00", jdTt:3184514, startJdTt:3184513.5, endJdTt:3184514.5,
+    researchResidualArcsec:0.009068677322278694,
+    start:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([207.341578,-0.00011340000000000001]), icrf:Object.freeze([179.26711346600001,0.36223660200000002]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([21.634897599999999,-0.76344460000000003]), icrf:Object.freeze([354.31653904799998,-3.3066621380000001]) }),
+    }),
+    truth:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([207.82900839999999,-0.00013329999999999999]), icrf:Object.freeze([179.715173021,0.17032889500000001]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([29.2289526,-1.4492674999999999]), icrf:Object.freeze([1.572663063,-0.95154808599999996]) }),
+    }),
+    end:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([208.31656699999999,-0.00015249999999999999]), icrf:Object.freeze([180.16333839999999,-0.021637633]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([36.803326400000003,-2.1073287000000001]), icrf:Object.freeze([8.7956958459999992,1.4115207590000001]) }),
+    }),
+  }),
+  Object.freeze({
+    label:"4006-11-17T12:00", jdTt:3184542, startJdTt:3184541.5, endJdTt:3184542.5,
+    researchResidualArcsec:0.0071232341210022559,
+    start:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([234.84975979999999,-0.0001315]), icrf:Object.freeze([204.818282119,-10.148899913999999]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([30.044225900000001,-1.5435589000000001]), icrf:Object.freeze([2.3582477110000002,-0.71777162000000005]) }),
+    }),
+    truth:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([235.34456789999999,-0.00014980000000000001]), icrf:Object.freeze([205.287941037,-10.325553336]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([37.5250561,-2.1740591]), icrf:Object.freeze([9.4854734789999995,1.630325553]) }),
+    }),
+    end:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([235.8395065,-0.0001671]), icrf:Object.freeze([205.758247606,-10.501571104]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([45.008927800000002,-2.7665858999999999]), icrf:Object.freeze([16.626060627000001,3.952943007]) }),
+    }),
+  }),
+  Object.freeze({
+    label:"4006-12-31T12:00", jdTt:3184586, startJdTt:3184585.5, endJdTt:3184586.5,
+    researchResidualArcsec:0.0030551453526784884,
+    start:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([278.88580139999999,8.8399999999999994e-05]), icrf:Object.freeze([249.173965982,-21.801866935]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([246.18660829999999,4.2190607]), icrf:Object.freeze([217.09241691700001,-10.001931046999999]) }),
+    }),
+    truth:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([279.39125389999998,9.7100000000000002e-05]), icrf:Object.freeze([249.71317506400001,-21.872096346999999]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([252.43248370000001,4.4883376999999998]), icrf:Object.freeze([223.219609421,-11.627071619000001]) }),
+    }),
+    end:Object.freeze({
+      sun:Object.freeze({ ecliptic:Object.freeze([279.8968036,0.00010399999999999999]), icrf:Object.freeze([250.25301021799999,-21.940584250000001]) }),
+      moon:Object.freeze({ ecliptic:Object.freeze([258.74865019999999,4.7054720000000003]), icrf:Object.freeze([229.48137728500001,-13.137546156999999]) }),
+    }),
+  }),
+]);
