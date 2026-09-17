bb0dd08001150b2bf34f2ae30ef20039e8cdc1eb
Mochi96336 <144137376+Mochi96336@users.noreply.github.com>
2026-09-18 01:51:46 +0800

    ux(research): load astronomy drilldown layer

--- src/four-pillar-determinacy-view.js
+++ src/four-pillar-determinacy-view.js
@@ -1,6 +1,7 @@
 import { fourPillarDeterminacy } from "./recurrence/four-pillar-determinacy.js";
 import "./day-hour-proof-chain-view.js";
 import "./research-evidence-drilldown-view.js";
+import "./research-astronomy-drilldown-view.js";
 import "./eot-observed-clearance-diagnostic-view.js";
 
 const instrument = document.querySelector("#recurrence-instrument");
